4f27d8c6fd466c16249de06dfbe9ff359361a0ca
Ristirianto Adi <ristiriantoadi@gmail.com>
2020-08-29 17:30:54 +0800

    done bill table

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -19,17 +19,17 @@
           <h2 class="card-title">Menu</h2>
           <ul class="nav nav-tabs">
             <li class="nav-item">
-              <a class="nav-link @if ($jenis === "makanan")
-                  active @endif" href="/menu/makanan">Makanan</a>
+              <a id="tab-makanan" class="nav-link @if ($jenis === "makanan")
+                  active @endif" href="#">Makanan</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link @if ($jenis === "minuman")
-              active @endif" href="/menu/minuman">Minuman</a>
+              <a id="tab-minuman" class="nav-link @if ($jenis === "minuman")
+              active @endif" href="#">Minuman</a>
             </li>
             </li>
           </ul>
-          <div class="menu-container">
-            @foreach($menus as $menu)
+          <div class="menu-container" id="menu">
+            {{-- @foreach($menus as $menu)
               <div class="card menu-item">
                 <div class="card-body">
                   <h3 class="card-title">{{$menu->nama}}</h3>
@@ -37,7 +37,7 @@
                   <button type="button" class="btn btn-info">Beli</button>
                 </div>
               </div>
-            @endforeach
+            @endforeach --}}
           </div>
         </div>
       </div>
@@ -61,9 +61,15 @@
 @endsection
 <script>
   // console.log(billTable);
+  var bills=[];
+  var makanan = <?php echo json_encode($makanan); ?>;
+  var minuman = <?php echo json_encode($minuman); ?>;
+  console.log(makanan);
+  console.log(minuman);
 
   function renderBill(bills){
     var billTable = document.getElementById("bill-table");
+    billTable.innerHTML="";
     bills.forEach((bill)=>{
       
       var tdNama = document.createElement('td');
@@ -81,10 +87,84 @@
     });
   }
 
+  // document.body.onload=()=>{
+  //   var bills = [{'nama':"bakso","porsi":2,"harga":10000},
+  //             {'nama':"nasi goreng","porsi":4,"harga":20000}]
+  //   renderBill(bills);
+  // }
+  
+  function renderMenu(subMenu){
+    var menu = document.getElementById("menu");
+    menu.innerHTML = "";
+
+    subMenu.forEach((item)=>{
+      var menuItem = document.createElement("div");
+      menuItem.classList.add("card");
+      menuItem.classList.add("menu-item");
+      menu.appendChild(menuItem)
+
+      var cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+      menuItem.appendChild(cardBody);
+
+      var namaMenu = document.createElement("h3");
+      namaMenu.classList.add("card-title");
+      namaMenu.innerHTML=item.nama;
+      cardBody.appendChild(namaMenu);
+      var hargaMenu = document.createElement("h5");
+      hargaMenu.classList.add("card-title");
+      hargaMenu.innerHTML=item.harga;
+      cardBody.appendChild(hargaMenu);
+
+      var buttonBeli = document.createElement("button");
+      buttonBeli.innerHTML="Beli";
+      buttonBeli.classList.add("btn");
+      buttonBeli.classList.add("btn-info");
+      buttonBeli.id = item.id;
+      console.log("halo");
+      buttonBeli.onclick=()=>{
+          var item = makanan.find((item)=>{
+            console.log("b");
+            if(item.id == buttonBeli.id){
+              return item;
+            }  
+          });
+          if (item == undefined){
+            item = minuman.find((item)=>{
+              console.log("undefined");
+              if(item.id == buttonBeli.id){
+                return item;
+              }  
+            });
+          }
+          
+          bills.push({"id":item.id,"nama":item.nama,"porsi":1,"harga":item.harga})
+          // console.log(bills);
+          renderBill(bills);
+      }
+
+
+      cardBody.appendChild(buttonBeli);
+    })
+
+  }
+
+
   document.body.onload=()=>{
-    var bills = [{'nama':"bakso","porsi":2,"harga":10000},
-              {'nama':"nasi goreng","porsi":4,"harga":20000}]
-    renderBill(bills);
+    var tabMakanan = document.getElementById('tab-makanan');
+    var tabMinuman = document.getElementById('tab-minuman');
+
+    tabMakanan.onclick=()=>{
+      renderMenu(makanan);
+      tabMakanan.classList.add("active");
+      tabMinuman.classList.remove("active");
+    }
+    tabMinuman.onclick=()=>{
+      renderMenu(minuman);
+      tabMinuman.classList.add("active");
+      tabMakanan.classList.remove("active");
+    }
+    renderMenu(makanan);
   }
 
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Menu;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,5 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/menu/makanan','MenuController@makanan');
-Route::get('/menu/minuman','MenuController@minuman');
+// Route::get('/menu/makanan','MenuController@makanan');
+// Route::get('/menu/minuman','MenuController@minuman');
+Route::get('/menu',function(){
+    $minuman = Menu::where('jenis', 'minuman')
+    ->get();
+    $makanan = Menu::where('jenis', 'makanan')
+    ->get();
+    $data=array(
+        'jenis'=>'makanan',
+        'minuman'=>$minuman,
+        'makanan'=>$makanan
+    );
+    return view('menu')->with($data);
+});
